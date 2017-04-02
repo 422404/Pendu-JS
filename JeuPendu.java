c29cc653a584fac4b8960a810e61763c60e3aa48
422404 <elyanpoujol@gmail.com>
2017-04-02 17:03:37 +0200

    Pas encore de pendu, les methodes s'etoffent

--- JeuPendu.java
+++ JeuPendu.java
@@ -24,6 +24,7 @@ public class JeuPendu {
     private static final int DIFFICILE = 2;
 
 
+
     /**
      * Lance une partie et demmande a en relancer
      * une autre a l'issue de la premiere
@@ -100,20 +101,93 @@ public class JeuPendu {
     
     /**
      * Tire aleatoirement un mot dans le tableau mots
+     * et construit le tableau du mot non complet a afficher
      * <p>Le tableau mots est un champs de classe</p>
      * @param difficultee Difficultee de la partie, influe
-     *                    sur le type de mot renvoyé
+     *                    sur le type de mot renvoye
      * @return Tableau de caracteres representant le mot
-     *         decoupe
+     *         decoupe, le tableau a deux dimensions,
+     *         une pour le mot entier et une pour le mot
+     *         avec certaines lettres remplacees par des '_'
      */
-    private static char[] trouverMot(int difficultee) {
-        char[] motChoisi = {'e', 'l', 'y', 'a', 'n'};
+    private static char[][] trouverMot(int difficultee) {
+        char[][] motChoisi = {{'e', 'l', 'y', 'a', 'n'},
+                              {'_', '_', 'y', '_', '_'}};
         // TODO finir le corps
         return motChoisi;
     }
     
     
     
+    /**
+     * Verifie que la lettre est contenue au moins une fois
+     * dans le mot choisi
+     * @param mot Tableau de caracteres representant le mot
+     *            choisi
+     * @param lettre Lettre a verifier
+     * @return True si la lettre y est presente sinon false
+     */
+    private static boolean verifierLettre(char[][] mot, char lettre) {
+        for (int indice = 0; indice < mot[0].length; indice++) {
+            // si au moins une lettre est presente on renvoie true
+            if (mot[0][indice] == lettre) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    
+    
+    /**
+     * Decouvre la lettre choisie du tableau de caracteres donne
+     * <p>La methode ne fais rien si la lettre n'est pas presente</p>
+     * @param mot Tableau de caracteres representant le mot
+     *            choisi
+     * @param lettre Lettre a decouvrir
+     */
+    private static void decouvrirLettre(char[][] mot, char lettre) {
+        
+        for (int indice = 0; indice < mot[0].length; indice++) {
+            // quand la lettre est presente dans le mot on 
+            // la place dans le mot cache
+            if (mot[0][indice] == lettre) {
+                mot[1][indice] = lettre;
+            }
+        }
+    }
+    
+    
+    
+    /**
+     * Demande la difficultee voulue a l'utilisaieur
+     * @return Un int representant la difficultee,
+     *         voir les champs de classe FACILE, MOYEN 
+     *         et DIFFICILE
+     */
+    private static int choisirDifficultee() {
+        // TODO finir le corps
+        return FACILE;
+    }
+    
+    
+    
+    /**
+     * Demande la saisie d'une lettre a verifier ulterieurement
+     * @return Caratere etant la lettre entree
+     */
+    private static char demanderlettre() {
+        String lettre = "\0";
+        
+        System.out.print("Veuillez saisir une lettre : ");
+        if (entree.hasNext("[a-zA-Z]")) {
+            lettre = entree.next();
+        }
+        entree.nextLine();
+        
+        return lettre.charAt(0);
+    }
     
     
     
